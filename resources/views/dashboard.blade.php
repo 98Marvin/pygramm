d27ffe8b1b056e942494bda5ffc467206a38f5dc
98Marvin <marvinwanyama99@gmail.com>
2023-03-02 13:40:51 +0300

    Read Users Data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{-- {{ __('Dashboard') }} --}}
+            Welcome... <b>{{ auth()->user()->name }}</b>
+
+
+            <b class="float-right">
+                <span class="badge badge-success">{{ $users->count() }}</span>Users
+            </b>
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+            <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Created At</th>
+                  </tr>
+                </thead>
+                <tbody>
+                @foreach ($users as $i=>$user)
+                    
+                <tr>
+                    <th scope="row">{{ ++$i }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at->diffForHumans() }}</td>
+                </tr>
+                @endforeach
+                </tbody>
+              </table> 
+        
         </div>
     </div>
 </x-app-layout>
